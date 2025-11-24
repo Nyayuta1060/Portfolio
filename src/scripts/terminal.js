66ff33e3297daa705037653cbcbeb9bb6d71bda4
Nyayuta <nyayuta.works@gmail.com>
2025-11-24 23:25:36 +0900

    fix:bannerのアスキーアートの幅を修正

--- src/scripts/terminal.js
+++ src/scripts/terminal.js
@@ -286,14 +286,13 @@ GitHub: ${project.github || 'N/A'}`;
     execute: () => {
       return `
 ╔═══════════════════════════════════════╗
-║　　　　　　　　　　　　　　　　　　　║
-║ Welcome to Nyayuta's Portfolio OS   ║
-║　　　　　　　　　　　　　　　　　　　║
-║ 大阪公立大学工業高等専門学校　　　 ║
-║ 知能情報コース 2年生　　　　　　　 ║
-║　　　　　　　　　　　　　　　　　　　║
+║                                       ║
+║ Welcome to Nyayuta's Portfolio OS     ║
+║                                       ║
+║ 大阪公立大学工業高等専門学校            　 ║
+║ 知能情報コース 2年生                     ║
+║                                       ║
 ╚═══════════════════════════════════════╝
-
 Type 'help' to see available commands`;
     }
   },
